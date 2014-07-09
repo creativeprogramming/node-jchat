4f627eb45f9affe6f77409fdce933d44ebfeef22
Michael Pfister <michael@mp-development.de>
2014-07-09 21:18:20 +0200

    added user name and timestamp

--- public/js/jquery.chat.js
+++ public/js/jquery.chat.js
@@ -6,30 +6,55 @@
             plug.defaults,
             options);
 
-        return this.each(function() {
+        return this.each(function () {
             var c = new _chat();
-            $(this).find('#chatForm').on('submit', function(e) {
+            $(this).find('#chatForm').on('submit', function (e) {
                 c.submitMsg(this, e);
             });
         })
     };
 
-    var _chat = function() {
+    var _chat = function () {
         this.socket = io();
-        this.user = null;
+        this.user = 'anonym';
 
-        this.socket.on('chat message', function(msg, user){
-            $('#messages').append($('<li>').text(msg));
-        });
+        this.initUser();
     }
 
     _chat.prototype = {
-        submitMsg: function(f, event) {
+        submitMsg: function (f, event) {
             event.preventDefault();
-            this.socket.emit('chat message', $('#m').val());
+            this.socket.emit('chat message', { time: this.getTimeString(new Date()), user: this.user, msg: $('#m').val() });
             $('#m').val('');
+            this.initChat();
 
             return false;
+        },
+        initChat: function () {
+            this.socket.on('chat message', function (data) {
+                $('#messages').append($('<li></li>')
+                    .append($('<span>').text(data.time), $('<b>').text(typeof(data.user) != 'undefined' ? data.user + ': ' : ''), $('<span>').text(data.msg)
+                    )
+                );
+            });
+        },
+        initUser: function () {
+            if ('anonym' === this.user) {
+                $('#user').show();
+                $('#userForm').on('submit', $.proxy(function (event) {
+                    event.preventDefault();
+                    this.user = $('#name').val();
+                    $('#user').hide();
+                }, this));
+            }
+        },
+        getTimeString: function(time) {
+            var time = new Date(time);
+            return '[' +
+                (time.getHours() < 10 ? '0' + time.getHours() : time.getHours())
+                + ':' +
+                (time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes())
+                + '] ';
         }
     }
 
