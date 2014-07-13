144b41b0812ee014bd7516daea77500efa3ba55c
Michael Pfister <michael@mp-development.de>
2014-07-13 13:24:23 +0200

    added caching of last messages for x time and send it to new connected users

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var formatDate = require('dateformat');
-var jchat = require('./lib/jchat');
 var config = require('./config.json');
+var jchat = require('./lib/jchat');
+var cache = require('./lib/cache');
+var jCache = new cache(config.cachetime);
 
 server.listen(config.port);
 
@@ -14,14 +16,21 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    jCache.clearCache();
+    Object.keys(jCache.get()).forEach(function(i) {
+        socket.emit('chat message', jCache.get()[i]);
+    });
+
     socket.emit('chat message', {
         time: formatDate(new Date(), config.timeformat),
         user: 'system',
         msg: config.welcomemsg
     });
+
     socket.on('chat message', function (msg, user) {
         msg.msg = jchat.BBcode(msg.msg);
         msg.time = formatDate(msg.time, config.timeformat);
+        jCache.set(msg);
         io.emit('chat message', msg);
     });
 });
